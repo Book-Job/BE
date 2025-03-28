b7e91a67bc610da491e89f50bfcff826ca837c0b
kim-youngchul <kmosh1212@naver.com>
2025-03-28 19:39:38 +0900

    feat: 프로젝트 초기화

---
+++ src/main/java/com/bookjob/BookjobApplication.java
@@ -0,0 +1,13 @@
+package com.bookjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookjobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookjobApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bookjob/BookjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookjobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
