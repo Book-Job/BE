ac43f8edc964a6e61ba02b528e08eff43494c42e
kim-youngchul <kmosh1212@naver.com>
2025-03-29 13:27:41 +0900

    test: softDelete 테스트 작성

---
+++ src/test/java/com/bookjob/common/domain/SoftDeleteTest.java
@@ -0,0 +1,54 @@
+package com.bookjob.common.domain;
+
+import com.bookjob.common.config.TestPasswordEncoderConfig;
+import com.bookjob.member.domain.Member;
+import com.bookjob.member.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@DataJpaTest
+@Import(TestPasswordEncoderConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class SoftDeleteTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    void softDelete() {
+        // given
+        Member member = Member.builder()
+                .email("email@email.com")
+                .loginId("loginId")
+                .password(Password.of("asdf", passwordEncoder))
+                .nickname("nickname")
+                .build();
+
+        member = memberRepository.save(member);
+
+        // when
+        memberRepository.delete(member);
+
+        Member deletedEntity = entityManager
+                .getEntityManager()
+                .createQuery("SELECT e FROM Member e WHERE e.id = :id", Member.class)
+                .setParameter("id", member.getId())
+                .getSingleResult();
+
+        // then
+        assertNotNull(deletedEntity.getDeletedAt());
+    }
+}
