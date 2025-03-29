8a9ad634142b9b010ed6822405e76eb54dccdaf8
kim-youngchul <kmosh1212@naver.com>
2025-03-29 17:21:11 +0900

    feat: 게시판 글 작성 비즈니스 로직 작성

---
+++ src/main/java/com/bookjob/board/service/BoardWriteService.java
@@ -0,0 +1,31 @@
+package com.bookjob.board.service;
+
+import com.bookjob.board.domain.Board;
+import com.bookjob.board.dto.BoardCreateRequest;
+import com.bookjob.board.repository.BoardRepository;
+import com.bookjob.member.domain.Member;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BoardWriteService {
+
+    private final BoardRepository boardRepository;
+
+    public void createBoard(BoardCreateRequest request, Member member) {
+        boolean isSameNickname = request.nickname().equals(member.getNickname());
+
+        Board board = Board.builder()
+                .title(request.title())
+                .isAuthentic(isSameNickname)
+                .text(request.text())
+                .nickname(isSameNickname ? member.getNickname() : request.nickname())
+                .memberId(member.getId())
+                .build();
+
+        boardRepository.save(board);
+    }
+}
