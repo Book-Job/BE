a0955f8ae8a05de22913091b9904438b5fc1922b
kim-youngchul <kmosh1212@naver.com>
2025-03-29 17:20:48 +0900

    feat: 게시판 도메인 작성

---
+++ src/main/java/com/bookjob/board/domain/Board.java
@@ -0,0 +1,68 @@
+package com.bookjob.board.domain;
+
+import com.bookjob.common.domain.SoftDeleteEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE board SET deleted_at = CURRENT_TIMESTAMP WHERE id = ?")
+public class Board extends SoftDeleteEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long memberId;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Lob
+    @Column(nullable = false)
+    private String text;
+
+    @Column(nullable = false)
+    private Long viewCount = 0L;
+
+    private Boolean isAuthentic;
+
+    private Long commentCount = 0L;
+
+    @Builder
+    public Board(Boolean isAuthentic, Long memberId, String nickname, String text, String title) {
+        this.isAuthentic = isAuthentic;
+        this.memberId = memberId;
+        this.nickname = nickname;
+        this.text = text;
+        this.title = title;
+    }
+
+    public boolean increaseViewCount() {
+        this.viewCount++;
+        return true;
+    }
+
+    public boolean increaseCommentCount() {
+        this.commentCount++;
+        return true;
+    }
+
+    public boolean decreaseViewCount() {
+        if (commentCount < 0) {
+            return false;
+        }
+
+        this.viewCount--;
+
+        return true;
+    }
+}
