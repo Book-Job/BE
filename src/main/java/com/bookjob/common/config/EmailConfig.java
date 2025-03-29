9cb77f0f7003c9e8298a837d551763d30b9c0da0
kuma0112 <lsj23871@gmail.com>
2025-03-29 15:47:55 +0900

    refactor: EmailConfig 수정
    - SSLSocketFactory 사용 삭제
    - debug 기능 off
    - 타임아웃 설정 추가

--- src/main/java/com/bookjob/common/config/EmailConfig.java
+++ src/main/java/com/bookjob/common/config/EmailConfig.java
@@ -31,11 +31,14 @@ public class EmailConfig {
         Properties mailProperties = new Properties();
         mailProperties.put("mail.transport.protocol", "smtp"); // 구글의 smtp 프로토콜로 전송
         mailProperties.put("mail.smtp.auth", "true"); // smtp 서버에 이메일을 보낼 때 인증 필요. 무단 액세스로부터 계정 보호
-        mailProperties.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); // 메일 전송에 ssl 기반 소켓 사용하는 설정
         mailProperties.put("mail.smtp.starttls.enable", "true"); // tls 관련 설정. 메일 클라이언트와 서버 사이 연결 암호화. 데이터 안전 전송
-        mailProperties.put("mail.smtp.debug", "true"); // 디버깅 가능
+        mailProperties.put("mail.smtp.debug", "false"); // 디버깅 가능
         mailProperties.put("mail.smtp.ssl.trust", "smtp.gmail.com"); // ssl 인증서 유효함을 인증
         mailProperties.put("mail.smtp.ssl.protocols", "TLSv1.2"); // tls 1.2 버전 사용하여 ssl 보안 프로토콜 지정
+
+        mailProperties.put("mail.smtp.connectiontimeout", "5000");
+        mailProperties.put("mail.smtp.timeout", "5000");
+        mailProperties.put("mail.smtp.writetimeout", "5000");
 
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setJavaMailProperties(mailProperties);
