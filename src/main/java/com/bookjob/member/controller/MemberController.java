2165e548a6606358b8bd841e75cc06b68cfe5ade
kuma0112 <lsj23871@gmail.com>
2025-03-29 10:16:20 +0900

    feat: Member 회원가입 구현

---
+++ src/main/java/com/bookjob/member/controller/MemberController.java
@@ -0,0 +1,27 @@
+package com.bookjob.member.controller;
+
+import com.bookjob.common.dto.CommonResponse;
+import com.bookjob.member.MemberFacade.MemberFacade;
+import com.bookjob.member.dto.MemberSignupRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberFacade memberFacade;
+
+    @PostMapping("/signup")
+    public ResponseEntity<CommonResponse<Void>> signup(@Valid @RequestBody MemberSignupRequest request) {
+        memberFacade.saveMember(request);
+
+        return ResponseEntity.ok(CommonResponse.success());
+    }
+}
