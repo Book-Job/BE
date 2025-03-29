bef9177a517ee8ca4f7817565f46309040b26aa7
kuma0112 <lsj23871@gmail.com>
2025-03-29 18:42:55 +0900

    fix: InternalServerError -> 적절한 exception 으로 반환하도록 수정

--- src/main/java/com/bookjob/common/exception/ConflictException.java
+++ src/main/java/com/bookjob/common/exception/ConflictException.java
@@ -5,18 +5,22 @@ import org.springframework.http.HttpStatus;
 public class ConflictException extends BaseException {
     static private final String DUPLICATED_EMAIL = "이미 가입된 회원입니다. 이메일: %s";
     static private final String DUPLICATED_LOGINID = "중복된 아이디입니다.";
+    static private final String DUPLICATED_NICKNAME = "중복된 닉네임입니다.";
 
     public ConflictException(String message) {
         super(message, HttpStatus.CONFLICT);
     }
 
-    public static InternalServerError duplicatedEmail(String email) {
+    public static ConflictException duplicatedEmail(String email) {
         String formattedMessage = String.format(DUPLICATED_EMAIL, email);
-        return new InternalServerError(formattedMessage);
+        return new ConflictException(formattedMessage);
     }
 
-    public static InternalServerError duplicatedLoginId() {
-        String formattedMessage = String.format(DUPLICATED_LOGINID);
-        return new InternalServerError(formattedMessage);
+    public static ConflictException duplicatedLoginId() {
+        return new ConflictException(DUPLICATED_LOGINID);
+    }
+
+    public static ConflictException duplicatedNickname() {
+        return new ConflictException(DUPLICATED_NICKNAME);
     }
 }
