4b644b9a948185c2560f00e520a6e9c1f3dab6b0
kim-youngchul <kmosh1212@naver.com>
2025-03-29 17:25:42 +0900

    feat: 활성유저 검색 비즈니스 로직 작성

--- src/main/java/com/bookjob/common/exception/UnAuthorizedException.java
+++ src/main/java/com/bookjob/common/exception/UnAuthorizedException.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 
 public class UnAuthorizedException extends BaseException {
     private final static String TOKEN_UNAUTHORIZED = "토큰 인증에 실패했습니다.";
+    private final static String DEACTIVATED = "삭제되었거나 정지 상태인 계정입니다.";
 
     public UnAuthorizedException(String message) {
         super(message, HttpStatus.UNAUTHORIZED);
@@ -11,5 +12,9 @@ public class UnAuthorizedException extends BaseException {
 
     public static UnAuthorizedException securityUnauthorized() {
         return new UnAuthorizedException(TOKEN_UNAUTHORIZED);
+    }
+
+    public static UnAuthorizedException deactivatedMemberUnauthorized() {
+        return new UnAuthorizedException(DEACTIVATED);
     }
 }
