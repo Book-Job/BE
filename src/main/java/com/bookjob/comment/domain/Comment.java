f5bb420c1ae2652dc9f2e0069f5d3d81ec03b1b2
kim-youngchul <kmosh1212@naver.com>
2025-04-01 19:20:36 +0900

    refactor: pk 자동생성 추가

--- src/main/java/com/bookjob/comment/domain/Comment.java
+++ src/main/java/com/bookjob/comment/domain/Comment.java
@@ -1,9 +1,7 @@
 package com.bookjob.comment.domain;
 
 import com.bookjob.common.domain.SoftDeleteEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,6 +12,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment extends SoftDeleteEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
