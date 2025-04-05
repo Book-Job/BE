949e2fe9a09cc5a391bc8472740fef9038243351
kim-youngchul <kmosh1212@naver.com>
2025-04-05 20:57:09 +0900

    feat: 댓글 조회 비즈니스 로직 작성

--- src/main/java/com/bookjob/comment/repository/CommentRepository.java
+++ src/main/java/com/bookjob/comment/repository/CommentRepository.java
@@ -1,7 +1,21 @@
 package com.bookjob.comment.repository;
 
 import com.bookjob.comment.domain.Comment;
+import com.bookjob.comment.dto.response.CommentResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @Query("""
+            SELECT new com.bookjob.comment.dto.response.CommentResponse(
+            c.id, c.text, c.nickname, c.isAuthentic, c.createdAt, c.modifiedAt)
+            FROM Comment c
+            WHERE c.boardId = :boardId AND c.id < :cursorId
+            ORDER BY c.createdAt DESC
+            LIMIT :limit
+            """)
+    List<CommentResponse> findCommentByBoardId(@Param("boardId") Long boardId, @Param("cursorId") Long cursorId, int limit);
 }
