5889164e544dba43d5a7353e320c5bb32e70eb58
kuma0112 <lsj23871@gmail.com>
2025-04-14 22:07:51 +0900

    feat: 구직 조회, 수정, 삭제 기능 추가

--- src/main/java/com/bookjob/job/facade/JobSeekingFacade.java
+++ src/main/java/com/bookjob/job/facade/JobSeekingFacade.java
@@ -1,6 +1,11 @@
 package com.bookjob.job.facade;
 
+import com.bookjob.job.domain.JobSeekingOrder;
 import com.bookjob.job.dto.request.JobSeekingCreateRequest;
+import com.bookjob.job.dto.request.JobSeekingUpdateRequest;
+import com.bookjob.job.dto.response.CursorJobSeekingResponse;
+import com.bookjob.job.dto.response.JobSeekingDetailsResponse;
+import com.bookjob.job.service.JobSeekingReadService;
 import com.bookjob.job.service.JobSeekingWriteService;
 import com.bookjob.member.domain.Member;
 import lombok.RequiredArgsConstructor;
@@ -11,8 +16,26 @@ import org.springframework.stereotype.Component;
 public class JobSeekingFacade {
 
     private final JobSeekingWriteService jobSeekingWriteService;
+    private final JobSeekingReadService jobSeekingReadService;
 
     public void createJobSeeking(JobSeekingCreateRequest request, Member member) {
         jobSeekingWriteService.createJobSeeking(request, member);
+    }
+
+    public CursorJobSeekingResponse getJobSeekings(String order, Long cursor, int pageSize) {
+        JobSeekingOrder jobSeekingOrder = JobSeekingOrder.fromString(order);
+        return jobSeekingReadService.getJobSeekings(jobSeekingOrder, cursor, pageSize);
+    }
+
+    public JobSeekingDetailsResponse getJobSeekingDetails(Long id) {
+        return jobSeekingReadService.getJobSeekingDetails(id);
+    }
+
+    public void updateJobSeeking(Long id, JobSeekingUpdateRequest request, Member member) {
+        jobSeekingWriteService.updateJobSeeking(id, request, member);
+    }
+
+    public void deleteJobSeeking(Long id, Member member) {
+        jobSeekingWriteService.deleteJobSeeking(id, member);
     }
 }
--- src/main/java/com/bookjob/job/service/JobSeekingWriteService.java
+++ src/main/java/com/bookjob/job/service/JobSeekingWriteService.java
@@ -1,9 +1,14 @@
 package com.bookjob.job.service;
 
+import com.bookjob.common.exception.ForbiddenException;
+import com.bookjob.common.exception.NotFoundException;
 import com.bookjob.job.domain.EmploymentType;
 import com.bookjob.job.domain.JobCategory;
+import com.bookjob.job.domain.JobPosting;
 import com.bookjob.job.domain.JobSeeking;
+import com.bookjob.job.dto.request.JobPostingUpdateRequest;
 import com.bookjob.job.dto.request.JobSeekingCreateRequest;
+import com.bookjob.job.dto.request.JobSeekingUpdateRequest;
 import com.bookjob.job.repository.JobSeekingRepository;
 import com.bookjob.member.domain.Member;
 import lombok.RequiredArgsConstructor;
@@ -33,5 +38,31 @@ public class JobSeekingWriteService {
                 .build();
 
         jobSeekingRepository.save(jobSeeking);
+    }
+
+    public void updateJobSeeking(Long id, JobSeekingUpdateRequest request, Member member) {
+        JobCategory jobCategory = JobCategory.fromString(request.jobCategory());
+        EmploymentType employmentType = EmploymentType.fromString(request.employmentType());
+
+        JobSeeking jobSeeking = jobSeekingRepository.findById(id).orElseThrow(NotFoundException::jobSeekingNotFound);
+
+        if (!jobSeeking.getMemberId().equals(member.getId())) {throw ForbiddenException.forbidden();}
+
+        jobSeeking.update(
+                request.title(),
+                request.text(),
+                employmentType,
+                jobCategory,
+                request.contactEmail(),
+                request.experience()
+        );
+    }
+
+    public void deleteJobSeeking(Long id, Member member) {
+        JobSeeking jobSeeking = jobSeekingRepository.findById(id).orElseThrow(NotFoundException::jobSeekingNotFound);
+
+        if (!jobSeeking.getMemberId().equals(member.getId())) {throw ForbiddenException.forbidden();}
+
+        jobSeekingRepository.delete(jobSeeking);
     }
 }
