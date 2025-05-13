d30740fa81b76e9fd0bf26ca2be22c50951630ba
kim-youngchul <kmosh1212@naver.com>
2025-05-13 20:08:23 +0900

    refactor: 댓글 조회 기능 작성자 필드 추가하여 쿼리 변경

--- src/main/java/com/bookjob/comment/repository/CommentRepository.java
+++ src/main/java/com/bookjob/comment/repository/CommentRepository.java
@@ -4,20 +4,21 @@ import com.bookjob.comment.domain.Comment;
 import com.bookjob.comment.dto.response.CommentResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Query("""
             SELECT new com.bookjob.comment.dto.response.CommentResponse(
-            c.id, c.text, c.nickname, c.isAuthentic, c.createdAt, c.modifiedAt)
+            c.id, c.text, c.nickname, c.isAuthentic, c.createdAt, c.modifiedAt,
+                        case when c.memberId = :memberId then true else false end
+                                    )
             FROM Comment c
             WHERE c.boardId = :boardId AND c.id < :cursorId AND c.deletedAt IS NULL
             ORDER BY c.createdAt DESC
             LIMIT :limit
             """)
-    List<CommentResponse> findCommentByBoardId(@Param("boardId") Long boardId, @Param("cursorId") Long cursorId, int limit);
+    List<CommentResponse> findCommentByBoardId(Long boardId, Long cursorId, int limit, Long memberId);
 
     void deleteAllByMemberId(Long memberId);
 }
