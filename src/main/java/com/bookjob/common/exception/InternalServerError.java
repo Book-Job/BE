9f515cf0e1c33d7044d7d9817337bd11cf804994
kim-youngchul <kmosh1212@naver.com>
2025-05-19 21:38:14 +0900

    feat: 에러 메세지 추가

--- src/main/java/com/bookjob/common/exception/InternalServerError.java
+++ src/main/java/com/bookjob/common/exception/InternalServerError.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 public class InternalServerError extends BaseException {
     static private final String EMAIL_SEND_ERROR = "이메일을 전송하던 중 문제가 발생했습니다. 이메일: %s";
     static private final String GENERATE_RANDOM_NUMBER_ERROR = "인증번호를 만들던 중 오류가 발생했습니다.";
+    static private final String NEGATIVE_COUNT_ERROR = "카운트 값이 음수가 될 수 없습니다. 요청된 값: %d";
 
     public InternalServerError(String message) {
         super(message, HttpStatus.INTERNAL_SERVER_ERROR);
@@ -19,4 +20,8 @@ public class InternalServerError extends BaseException {
         return new InternalServerError(GENERATE_RANDOM_NUMBER_ERROR);
     }
 
+    public static InternalServerError negativeCountError(Long count) {
+        String formattedMessage = String.format(NEGATIVE_COUNT_ERROR, count);
+        return new InternalServerError(formattedMessage);
+    }
 }
